6a391547c78b46a99e412dacd2c8783d6d013e82
Andrej Zevzikov <andrejzevz13@gmail.com>
2022-07-11 01:11:09 +0300

    second commit for testing

--- src/main/java/com/final_project/loans/LoansApplication.java
+++ src/main/java/com/final_project/loans/LoansApplication.java
@@ -8,7 +8,7 @@ public class LoansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoansApplication.class, args);
-		System.out.println("Hello world");
+		System.out.println("Hello world ");
 	}
 
 }
